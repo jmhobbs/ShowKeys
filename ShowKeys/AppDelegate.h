7c8c2f11aba9ebd6d3205f904b1dad48b1a9d2c7
John Hobbs <john@velvetcache.org>
2015-06-23 16:33:04 -0500

    Initial Commit

---
+++ ShowKeys/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShowKeys
+//
+//  Created by John Hobbs on 6/23/15.
+//  Copyright (c) 2015 John Hobbs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ShowKeys/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ShowKeys
+//
+//  Created by John Hobbs on 6/23/15.
+//  Copyright (c) 2015 John Hobbs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
